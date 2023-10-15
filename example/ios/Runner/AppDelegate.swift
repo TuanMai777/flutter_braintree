9137d17e6cd4132399a421deefba06070d0a817e
Dishant Mahajan <mr.dishantmahajan@gmail.com>
2023-10-15 10:28:40 +0200

    Paypal ios fixed

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Braintree
     
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
       GeneratedPluginRegistrant.register(with: self)
-      BTAppSwitch.setReturnURLScheme("in.appyflow.flutterBraintreeExample")
+//      BTAppSwitch.setReturnURLScheme("in.appyflow.flutterBraintreeExample")
       
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
@@ -15,7 +15,7 @@ import Braintree
     
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         if url.scheme == "in.appyflow.flutterBraintreeExample" {
-            return BTAppSwitch.handleOpen(url, options:options)
+//            return BTAppSwitch.handleOpen(url, options:options)
         }
         
         return false
